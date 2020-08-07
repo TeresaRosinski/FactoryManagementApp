a8dae03ddc3ebca2ab9b1ad3a28b114fb7a8bdab
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-08-07 13:46:42 -0500

    add index controller

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+    public MachinesController (FactoryContext db)
+    {
+      _db=db;
+    }
+
+    public ActionResult Index ()
+    {
+      List<Machine>model = _db.Machines.ToList();
+      return View (model);
+    }
+  }
+}
